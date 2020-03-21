e761559fa09bdf8b0b51d12dfa9c714f517250b2
badyanov <d.badyanov@outlook.com>
2020-03-22 01:12:54 +0300

    вводятся и выводятся сообщения. На форму выведено поле для поиска (не работает)

--- src/main/java/ru/badyanov/spring/GreetingController.java
+++ src/main/java/ru/badyanov/spring/GreetingController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.badyanov.spring.domain.Message;
 import ru.badyanov.spring.repos.MessageRepo;
@@ -16,7 +17,6 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
-
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
                            Map<String, Object> model) {
@@ -27,8 +27,21 @@ public class GreetingController {
     @GetMapping
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
-
         model.put("messages", messages);
+
+        return "main";
+    }
+
+    // Урок № 2 - обработка ввода данных
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        //ToDo: пока что скопировал 2 строчки из метода main. Потом переделать.
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
         return "main";
     }
 }
